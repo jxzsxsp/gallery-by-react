c7253e5d3ace5feeb4f10d7a81b3a09e1d256fdd
Sean <sean.xiao@lianshang.cn>
2016-05-21 16:18:11 +0800

    format code

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,27 +6,26 @@ import React from 'react';
 //获取图片数据
 let imageDatas = require('../data/imageDatas.json');
 //利用自执行函数，将图片名信息转成图片URL信息
-imageDatas = (function genImageDatas(imageDataArr){
-  for (let i=0,j=imageDataArr.length;i<j;i++){
-    let singleImageData = imageDataArr[i];
-    singleImageData.imageUrl = require('../images/'+singleImageData.fileName);
-    imageDataArr[i] = singleImageData;
-  }
-  return imageDataArr;
+imageDatas = (function genImageDatas(imageDataArr) {
+    for (let i = 0, j = imageDataArr.length; i < j; i++) {
+        let singleImageData = imageDataArr[i];
+        singleImageData.imageUrl = require('../images/' + singleImageData.fileName);
+        imageDataArr[i] = singleImageData;
+    }
+    return imageDataArr;
 })(imageDatas);
 
 class AppComponent extends React.Component {
-  render() {
-    return (
-      <section className="stage">
-        <section className="img-sec"></section>
-        <nav className="controller-nav"></nav>
-      </section>
-    );
-  }
+    render() {
+        return (
+            <section className="stage">
+                <section className="img-sec"></section>
+                <nav className="controller-nav"></nav>
+            </section>
+        );
+    }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
