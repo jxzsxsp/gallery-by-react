d06d1e66e69fcf38cbdd9bacf6a1acbf9779ea75
Sean <sean.xiao@lianshang.cn>
2016-05-23 11:24:17 +0800

    完成画廊应用

---
+++ src/components/ControllerUnit.js
@@ -0,0 +1,45 @@
+'use strict';
+
+import React,{Component} from 'react';
+
+require('styles/ControllerUnit.scss');
+
+// 控制组件
+export default class ControllerUnit extends Component {
+
+    handleClick(e) {
+        // 如果点击的是当前正在选中态的按钮，则翻转图片，否则将对应的图片居中
+        if (this.props.arrange.isCenter) {
+            this.props.inverse();
+        } else {
+            this.props.center();
+        }
+
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    render() {
+        let controlelrUnitClassName = 'controller-unit';
+
+        // 如果对应的是居中的图片，显示控制按钮的居中态
+        if (this.props.arrange.isCenter) {
+            controlelrUnitClassName += ' is-center';
+
+            // 如果同时对应的是翻转图片， 显示控制按钮的翻转态
+            if (this.props.arrange.isInverse) {
+                controlelrUnitClassName += ' is-inverse';
+            }
+        }
+
+        return (
+            <span className={controlelrUnitClassName} onClick={this.handleClick.bind(this)}></span>
+        );
+    }
+}
+
+ControllerUnit.displayName = 'ControllerUnit';
+
+// Uncomment properties you need
+// ControllerUnitComponent.propTypes = {};
+// ControllerUnitComponent.defaultProps = {};
--- src/components/ImgFigure.js
+++ src/components/ImgFigure.js
@@ -1,25 +1,65 @@
 'use strict';
 
-import React from 'react';
+import React,{Component} from 'react';
 
 require('styles/ImgFigure.scss');
 
-export default class ImgFigure extends React.Component {
-  render() {
-    let styleObject = {};
-    if(this.props.arrange.pos){
-      styleObject = this.props.arrange.pos;
+export default class ImgFigure extends Component {
+
+    /*
+     * imgFigure 的点击处理函数
+     */
+    handleClick(e) {
+        if (this.props.arrange.isCenter) {
+            this.props.inverse();
+        } else {
+            this.props.center();
+        }
+
+        e.stopPropagation();
+        e.preventDefault();
     }
 
-    return (
-      <figure className="img-figure" style={styleObject}>
-        <img src={this.props.data.imageUrl} alt={this.props.data.title}/>
-        <figcaption>
-          <h2 className="img-title">{this.props.data.title}</h2>
-        </figcaption>
-      </figure>
-    );
-  }
+    render() {
+        let styleObj = {};
+
+        // 如果props属性中指定了这张图片的位置，则使用
+        if (this.props.arrange.pos) {
+            styleObj = this.props.arrange.pos;
+        }
+
+        // 如果图片的旋转角度有值并且不为0， 添加旋转角度
+        if (this.props.arrange.rotate) {
+            (['MozTransform', 'msTransform', 'WebkitTransform', 'transform']).forEach(function (value) {
+                styleObj[value] = 'rotate(' + this.props.arrange.rotate + 'deg)';
+            }.bind(this));
+        }
+
+        // 如果是居中的图片， z-index设为11
+        if (this.props.arrange.isCenter) {
+            styleObj.zIndex = 11;
+        }
+
+        let imgFigureClassName = 'img-figure';
+        imgFigureClassName += this.props.arrange.isInverse ? ' is-inverse' : '';
+
+        return (
+            <figure className={imgFigureClassName} style={styleObj} onClick={this.handleClick.bind(this)}>
+                <img src={this.props.data.imageURL}
+                     alt={this.props.data.title}
+                    />
+                <figcaption>
+                    <h2 className="img-title">{this.props.data.title}</h2>
+
+                    <div className="img-back" onClick={this.handleClick.bind(this)}>
+                        <p>
+                            {this.props.data.desc}
+                        </p>
+                    </div>
+                </figcaption>
+            </figure>
+        );
+    }
 }
 
 ImgFigure.displayName = 'ImgFigure';
